8d0d56a221c3bd6f7f04c65dfafde34a287fa2f0
RheoCents <centinaraphael0157@gmail.com>
2024-01-06 19:53:44 +0800

    Code update

--- max_number_checker.py
+++ max_number_checker.py
@@ -4,14 +4,20 @@
 # Ask user for 3 numbers
 
 print("Hello and welcome to Rheo's number checker\nFor this, we will ask you 3 numbers and it we will show the highest!")
-user_inputed_number_1 = int(input("give me a number "))
-user_inputed_number_2 = int(input("give me another "))
-user_inputed_number_3 = int(input("give me the last one "))
+
+user_input_number_1 = int(input("give me a number "))
+user_input_number_2 = int(input("give me another "))
+user_input_number_3 = int(input("give me the last one "))
 
 # Check the 3 numbers
-if user_inputed_number_3 > user_inputed_number_2 and user_inputed_number_1:
-    print (user_inputed_number_3 + " is the highest number")
-if user_inputed_number_2 > user_inputed_number_3 and user_inputed_number_1:
-    print (user_inputed_number_2 + " is the highest number")
+if user_input_number_3 > user_input_number_2 and user_input_number_1:
+    print (int(user_input_number_3) + str(" is the highest number"))
+if user_input_number_2 > user_input_number_3 and user_input_number_1:
+    print (int(user_input_number_2) + str(" is the highest number"))
+if user_input_number_1 > user_input_number_3 and user_input_number_2:
+    print(int(user_input_number_1) + str(" is the highest number"))
+if user_input_number_1 == user_input_number_3 and user_input_number_2:
+    print(int(user_input_number_1) + str(" is the highest number"))
+
 else:
-    print (user_inputed_number_1 + " is the highest number")
+    print ("That is not a number")
