7aa73edfeb98b58a808756f6f52415a446727a90
unknown <centinaraphael0157@gmail.com>
2024-01-06 19:01:26 +0800

    testing commit

--- max_number_checker.py
+++ max_number_checker.py
@@ -1,3 +1,14 @@
-#pseudo code test upload"
-#testing git commit
+# pseudo code test upload"
+# testing git commit
 
+# Ask user for 3 numbers
+# Check the 3 numbers
+# 
+a = input("give me a number")
+if a == 34:
+    print ("avc") 
+
+else:
+    print("not 34")
+    
+return
