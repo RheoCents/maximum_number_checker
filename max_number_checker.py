7047d2f1e827b216394440aad4f27277b3052727
RheoCents <centinaraphael0157@gmail.com>
2024-01-06 19:16:23 +0800

    Saving just in case

--- max_number_checker.py
+++ max_number_checker.py
@@ -4,13 +4,14 @@
 # Ask user for 3 numbers
 
 print("Hello and welcome to Rheo's number checker\nFor this, we will ask you 3 numbers and it we will show the highest!")
-user_inputed_number_1 = input("give me a number")
-user_inputed_number_2 = input("give me another")
-user_inputed_number_3 = input("give me the last one")
+user_inputed_number_1 = int(input("give me a number "))
+user_inputed_number_2 = input("give me another ")
+user_inputed_number_3 = input("give me the last one ")
 
 # Check the 3 numbers
+if user_inputed_number_3 > user_inputed_number_2 and user_inputed_number_1:
+    print (user_inputed_number_3 + str(" is the highest number"))
 if user_inputed_number_2 > user_inputed_number_3 and user_inputed_number_1:
-    print (user_inputed_number_2 + str(" is the highest number")) 
-
+    print (user_inputed_number_2 + str(" is the highest number"))
 else:
-    print("not 34")
+    print (user_inputed_number_1 + str(" is the highest number"))
