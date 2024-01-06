a9d596d8be8d99ba4f74cb834b56d0907377c0c5
unknown <centinaraphael0157@gmail.com>
2024-01-06 19:05:10 +0800

    uploading base code

--- max_number_checker.py
+++ max_number_checker.py
@@ -2,13 +2,15 @@
 # testing git commit
 
 # Ask user for 3 numbers
+
+print("Hello and welcome to Rheo's number checker\nFor this, we will ask you 3 numbers and it we will show the highest!")
+user_inputed_number_1 = input("give me a number")
+user_inputed_number_2 = input("give me another")
+user_inputed_number_3 = input("give me the last one")
+
 # Check the 3 numbers
-# 
-a = input("give me a number")
-if a == 34:
-    print ("avc") 
+if user_inputed_number_2 > user_inputed_number_3 and user_inputed_number_1:
+    print (user_inputed_number_2 + str(" is the highest number")) 
 
 else:
     print("not 34")
-    
-return
