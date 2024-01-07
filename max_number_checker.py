2dab29b71097be2a2b02f8e59950dd3f8f78898e
RheoCents <centinaraphael0157@gmail.com>
2024-01-07 18:08:13 +0800

    Making interpretation more simple and adding float value checker

--- max_number_checker.py
+++ max_number_checker.py
@@ -1,4 +1,8 @@
+
 def rheo_maximum_number_checker():
+    # Welcome
+    print("Hello and welcome to Rheo's number checker\nFor this, we will ask you 3 numbers and it we will show the highest!")
+    # Input
     user_input_number_1 = float(input("give me a number "))
     user_input_number_2 = float(input("give me another "))
     user_input_number_3 = float(input("give me the last one "))
@@ -11,7 +15,22 @@ def rheo_maximum_number_checker():
         #maximum checker if all are numbers
     if user_input_number_1 > user_input_number_2 and user_input_number_3:
           print(user_input_number_1, " is the highest value")
-    
     if user_input_number_2 > user_input_number_1 and user_input_number_3:
-          print(user_input_number_1, " is the highest value")
+          print(user_input_number_2, " is the highest value")
+    if user_input_number_3 > user_input_number_1 and user_input_number_2:
+          print(user_input_number_3, " is the highest value")
+    
+    else:
+         print("That is not a number you silly")
+
+
+        #repeat prompt
+    print("Would you like to try again? Y/N")
+    repeat_rheo_number_checker = input().lower()
+    if repeat_rheo_number_checker == "Y" or "y":
+        print()
+        rheo_maximum_number_checker()
+    else:
+        print("Thank you for using Rheo's maximum number checker for threes!")          
+
 rheo_maximum_number_checker()
