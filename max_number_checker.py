efbad5498a3a0fe370e1fb2b66db81dc137165a2
RheoCents <centinaraphael0157@gmail.com>
2024-01-07 11:41:55 +0800

    Validation comment check and added validation upload

--- max_number_checker.py
+++ max_number_checker.py
@@ -14,47 +14,71 @@ def rheo_number_checker():
         user_input_number_2 = float(user_input_number_2)
         user_input_number_3 = float(user_input_number_3)
 
-    # Check the 3 numbers
-        if user_input_number_1 == user_input_number_2 > user_input_number_3:
-            print("Input 1 and 2 have the same value")
+    # Check if the 3 numbers are the same
+        if user_input_number_1 == user_input_number_2 == user_input_number_3:
+            print("Input 1, 2, and 3 have the same value")
             print(user_input_number_1, " is the highest value")
+
+        # Check if 2 of the numbers are the same
         elif user_input_number_2 == user_input_number_3 > user_input_number_1:
             print("Input 2 and 3 have the same value")
             print(user_input_number_2, " is the highest value")
         elif user_input_number_3 == user_input_number_1 > user_input_number_2:
             print("Input 1 and 3 have the same value")
-            print(user_input_number_1, " has the highest value")
+            print(user_input_number_1, " is the highest value")
+        elif user_input_number_2 == user_input_number_1 > user_input_number_2:
+            print("Input 1 and 3 have the same value")
+            print(user_input_number_1, " is the highest value")
+
+        # Check which number is the highest
         elif user_input_number_3 > user_input_number_2 and user_input_number_1:
-            print (user_input_number_3, " has the highest value")
+            print (user_input_number_3, " is the highest value")
         elif user_input_number_2 > user_input_number_3 and user_input_number_1:
-            print (user_input_number_2, " has the highest value")
+            print (user_input_number_2, " is the highest value")
         elif user_input_number_1 > user_input_number_3 and user_input_number_2:
-            print(user_input_number_1, " has the highest value")
+            print(user_input_number_1, " is the highest value")
     else:
 
     # Checking if user input 1, 2, or 3 is a number
         if not user_input_number_1.isdigit() and not user_input_number_2.isdigit() and not user_input_number_3.isdigit(): 
                 print("Input number 1, 2, and 3 is not a number")
+                print("no input have the highest value")
             
-            #Cheking is 2 at the same time is a number
+            #Cheking is 2 inputs at the same time are not a number
         elif not user_input_number_1.isdigit() and not user_input_number_2.isdigit():
-                print("Input number 1 and 2 are not numbers")
+                print("Input number 1 and 2 are not numbers") 
+                print(user_input_number_3, " is the highest value")
         elif not user_input_number_2.isdigit() and not user_input_number_3.isdigit(): 
                 print("Input number 2 and 3 are not numbers")
+                print(user_input_number_1, " is the highest value")
         elif not user_input_number_1.isdigit() and not user_input_number_3.isdigit(): 
                 print("Input number 1 and 3 are not numbers")
+                print(user_input_number_2, " is the highest value")
             
             # Checking if one of them is a number
         elif not user_input_number_1.isdigit():
-                print("Input number 1 is not a number")
+                if user_input_number_2 > user_input_number_3:
+                    print("Input number 1 is not a number")
+                    print(user_input_number_2, " is the highest value")
+                elif user_input_number_3 > user_input_number_2:
+                    print("Input number 1 is not a number")
+                    print(user_input_number_3, "is the highest value")
+
         elif not user_input_number_2.isdigit():
-                print("Input number 2 is not a number")
+                 if user_input_number_2 > user_input_number_3:
+                    print("Input number 1 is not a number")
+                    print(user_input_number_2, " is the highest value")
+                elif user_input_number_3 > user_input_number_2:
+                    print("Input number 1 is not a number")
+                    print(user_input_number_3, "is the highest value")
+                
+
         elif not user_input_number_3.isdigit():
                 print("Input number 3 is not a number")
 
     print("Would you like to try again? Yes/No")
     repeat_rheo_number_checker = str(input())
-    if  repeat_rheo_number_checker == str("Yes" or "yes"):
+    if  repeat_rheo_number_checker.lower == str("Yes" or "yes"):
           rheo_number_checker()
 
 rheo_number_checker()
